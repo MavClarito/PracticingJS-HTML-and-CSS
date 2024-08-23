0e999cb7fe474d091b4b72afe1b723b5450809de
Mav <119854736+MavClarito@users.noreply.github.com>
2024-08-23 08:04:41 +0800

    Add files via upload

---
+++ Contact List/app.js
@@ -0,0 +1,14 @@
+﻿var name1 = "Maxwell Wright";
+var phone1 = "(0191) 719 6495";
+var email1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+
+var name2 = "Raja Villarreal";
+var phone2 = "0866 398 2895";
+var email2 = "posuere.vulputate@sed.com";
+
+var name3 = "Helen Richards";
+var phone3 = "0800 1111";
+var email3 = "libero@convallis.edu";
+
+console.log(`First contact - Name: ${name1}, Phone: ${phone1}, Email: ${email1}`);
+console.log(`Last contact - Name: ${name3}, Phone: ${phone3}, Email: ${email3}`);

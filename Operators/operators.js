337c97e64f00c08614e0cc16c692b85bf81f2cab
Mav <119854736+MavClarito@users.noreply.github.com>
2024-08-23 10:03:47 +0800

    Update operators.js

--- Operators/operators.js
+++ Operators/operators.js
@@ -2,13 +2,13 @@ console.log(2 + 3 * 1);       // expected 7
 console.log(2 ** 4);          // expected 16
 console.log(5 % 1);           // expected 5
 console.log(8 * 2 + 5 - 2);   // expected 39
-
+console.log("___________________________");
 console.log(4 * 5 === 20);    // true
 console.log(6 * 5 == "30");   // true
 console.log(-17 < 0);         // true
 console.log(25 > 1);          // true
 console.log(2 + 2 * 2 === 4); // true
-
+console.log("___________________________");
 console.log(true || false);             // true
 console.log(false || false);            // false
 console.log(false || false || true);    // true
